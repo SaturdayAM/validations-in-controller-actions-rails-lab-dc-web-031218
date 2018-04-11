e59a826638745cd97724d55bed9d5990ebeb40f1
SaturdayAM <jwl62@georgetown.edu>
2018-04-11 16:34:03 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+	validates(:title, {presence: :true})
+	validates :content, length: {minimum: 100}
+	validate :valid_category
+	
+	def valid_category
+		if !["Fiction", "Non-Fiction"].include? category
+			errors.add(:category, "Invalid category")
+		end
+	end
 end
+
